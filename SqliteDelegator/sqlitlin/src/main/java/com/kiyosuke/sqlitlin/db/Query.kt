3438d3c29920310cfefcb2c11e854ba3f0d3c155
kiyosuke <k.kiyosuke1999@gmail.com>
2019-02-13 16:59:08 +0900

    GROUP BY and HAVING query added.

--- SqliteDelegator/sqlitlin/src/main/java/com/kiyosuke/sqlitlin/db/Query.kt
+++ SqliteDelegator/sqlitlin/src/main/java/com/kiyosuke/sqlitlin/db/Query.kt
@@ -14,6 +14,9 @@ class Select<T : Table>(private val columns: List<Column<*>>, private val from: 
 
     private var where: Where? = null
 
+    private var groupBy: GroupBy? = null
+    private var having: Having? = null
+
     private var orderBy: OrderBy? = null
 
     private var limit: Limit? = null
@@ -25,6 +28,21 @@ class Select<T : Table>(private val columns: List<Column<*>>, private val from: 
 
     infix fun orderBy(sort: Pair<Column<*>, OrderBy.SortOrder>) {
         this.orderBy = OrderBy(sort.first, sort.second)
+    }
+
+    fun groupBy(vararg columns: Column<*>): GroupBy {
+        this.groupBy = GroupBy(listOf(*columns))
+        return groupBy!!
+    }
+
+    infix fun GroupBy.having(op: WhereOperationBuilder.(T) -> Op) {
+        val having = Having(op(WhereOperationBuilder, from))
+        this@Select.having = having
+    }
+
+    infix fun groupBy(columns: List<Column<*>>): GroupBy {
+        this.groupBy = GroupBy(columns)
+        return groupBy!!
     }
 
     infix fun limit(limit: Int) {
@@ -41,6 +59,13 @@ class Select<T : Table>(private val columns: List<Column<*>>, private val from: 
         where?.let {
             append(" WHERE ${it.whereOp.toSql()}")
         }
+        groupBy?.let {
+            append(" GROUP BY ")
+            append(it.columns.joinToString(",") { column -> column.tableName + "." + column.name })
+        }
+        having?.let {
+            append(" HAVING ${it.op.toSql()}")
+        }
         orderBy?.let {
             append(" ORDER BY ${it.column.tableName}.${it.column.name} ${it.sortOrder.name}")
         }
@@ -53,6 +78,10 @@ class Select<T : Table>(private val columns: List<Column<*>>, private val from: 
 val Column<*>.cursorKey: String get() = this.tableName + "_" + this.name
 
 data class Where(val whereOp: Op)
+
+data class GroupBy(val columns: List<Column<*>>)
+
+data class Having(val op: Op)
 
 data class OrderBy(val column: Column<*>, val sortOrder: SortOrder) {
     enum class SortOrder {
