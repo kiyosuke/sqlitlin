2ef9f797f78b117912a1636f62a8c11ce95856d7
kiyosuke <k.kiyosuke1999@gmail.com>
2019-02-28 14:40:03 +0900

    "MAX" and "MIN" were added to the conditional expressions.

--- SqliteDelegator/sqlitlin/src/main/java/com/kiyosuke/sqlitlin/db/WhereOperationBuilder.kt
+++ SqliteDelegator/sqlitlin/src/main/java/com/kiyosuke/sqlitlin/db/WhereOperationBuilder.kt
@@ -10,21 +10,32 @@ object WhereOperationBuilder {
     infix fun <T> Column<T>.neq(data: T): Op =
         if (this is Column.Text) Neq<String>(this, "'$data'") else Neq(this, data)
 
-    infix fun <T> Column<T>.less(data: T): Op = Less(this, data)
+    infix fun <T> Column<T>.less(data: T): Op =
+        if (this is Column.Text) Less<String>(this, "'$data'") else Less(this, data)
 
-    infix fun <T> Column<T>.lessEq(data: T): Op = LessEq(this, data)
+    infix fun <T> Column<T>.lessEq(data: T): Op =
+        if (this is Column.Text) LessEq<String>(this, "'$data'") else LessEq(this, data)
 
-    infix fun <T> Column<T>.greater(data: T): Op = Greater(this, data)
+    infix fun <T> Column<T>.greater(data: T): Op =
+        if (this is Column.Text) Greater<String>(this, "'$data'") else Greater(this, data)
 
-    infix fun <T> Column<T>.greaterEq(data: T): Op = GreaterEq(this, data)
+    infix fun <T> Column<T>.greaterEq(data: T): Op =
+        if (this is Column.Text) GreaterEq<String>(this, "'$data'") else GreaterEq(this, data)
 
-    infix fun Column.Text.like(text: String): Op = Like(this, text)
+    fun Column.Text.like(text: String, escape: String? = null): Op = Like(this, text, escape)
 
-    infix fun <T> Column<T>.between(between: Pair<T, T>): Op = Between(this, between)
+    infix fun <T> Column<T>.between(between: Pair<T, T>): Op =
+        if (this is Column.Text) Between<String>(this, "'${between.first}'" to "'${between.second}'") else Between(this, between)
 
-    infix fun <T> Column<T>.inList(list: List<T>): Op = InList(this, list)
+    infix fun <T> Column<T>.inList(list: List<T>): Op =
+        if (this is Column.Text) InList<String>(this, list.map { "'$it'" }) else InList(this, list)
 
-    infix fun <T> Column<T>.notInList(list: List<T>): Op = NotInList(this, list)
+    infix fun <T> Column<T>.notInList(list: List<T>): Op =
+        if (this is Column.Text) NotInList<String>(this, list.map { "'$it'" }) else NotInList(this, list)
+
+    infix fun <T> max(column: Column<T>): Op = OpMax(column)
+
+    infix fun <T> min(column: Column<T>): Op = OpMin(column)
 
     infix fun Op.or(operation: Op): Op = Or(this, operation)
 
@@ -68,9 +79,12 @@ class GreaterEq<T>(private val column: Column<T>, private val data: T) : Op() {
     }
 }
 
-class Like(private val column: Column.Text, private val data: String) : Op() {
+class Like(private val column: Column.Text, private val data: String, private val escape: String? = null) : Op() {
     override fun toSql(): String = buildString {
-        append("${column.tableName}.${column.name} LIKE '$data'")
+        append("${column.tableName}.${column.name}")
+        append(" LIKE ")
+        append("'$data'")
+        if (escape != null) append(" ESCAPE '$escape'")
     }
 }
 
@@ -97,6 +111,26 @@ class NotInList<T>(private val column: Column<T>, private val list: List<T>) : O
         return buildString {
             append("${column.tableName}.${column.name} NOT IN(")
             append(list.joinToString(", "))
+            append(")")
+        }
+    }
+}
+
+class OpMax<T>(private val column: Column<T>) : Op() {
+    override fun toSql(): String {
+        return buildString {
+            append("MAX(")
+            append("${column.tableName}.${column.name}")
+            append(")")
+        }
+    }
+}
+
+class OpMin<T>(private val column: Column<T>) : Op() {
+    override fun toSql(): String {
+        return buildString {
+            append("MIN(")
+            append("${column.tableName}.${column.name}")
             append(")")
         }
     }
