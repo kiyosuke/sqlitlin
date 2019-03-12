d6eabe1d60640d8c6f3e290c0f60915571661a6e
kiyosuke <k.kiyosuke1999@gmail.com>
2019-03-13 08:20:29 +0900

    メインスレッドからの実行を許可する設定を追加

--- SqliteDelegator/app/src/main/java/com/kiyosuke/sqlitedelegator/db/AppDatabase.kt
+++ SqliteDelegator/app/src/main/java/com/kiyosuke/sqlitedelegator/db/AppDatabase.kt
@@ -4,12 +4,15 @@ import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import com.kiyosuke.sqlitedelegator.db.dao.UserDao
 import com.kiyosuke.sqlitlin.db.core.NormalDatabase
-import com.kiyosuke.sqlitlin.db.core.SupportDatabase
 
 class AppDatabase(context: Context, name: String, version: Int) : NormalDatabase(context, name, version) {
 
     override fun createTable(db: SQLiteDatabase) {
         db.execSQL(Users.createSql)
+    }
+
+    override fun migration(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        // migration処理
     }
 
     val userDao by lazy { UserDao(this) }
